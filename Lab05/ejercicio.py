21fcada7174a01952d928344e655346e25e5803a
Ernesto Molina Anavisca (24247) <ernesto.molina@est.zamorano.edu>
2022-10-21 18:24:38 -0600

    latest

---
+++ Lab05/ejercicio.py
@@ -0,0 +1,9 @@
+try:
+    archivo= input("Ingrese el nombre del archivo: ")
+    f = open(archivo, "r")
+    for linea in f: 
+        print(linea.upper())
+except:
+        print("Error, archivo no encontrado")
+
+#print(fhandle.read().upper())
---
+++ pagoporhora.py
@@ -0,0 +1,6 @@
+#Tendencias e Innovacion en Tecnologia Agricola (TEA)
+
+horasTrabajadas= input("Ingrese las horas trabajadas: ")
+valorHora= input("ingrese el valor por hora: ")
+pagoHora= int(horasTrabajadas) * float(valorHora)
+print(pagoHora)
